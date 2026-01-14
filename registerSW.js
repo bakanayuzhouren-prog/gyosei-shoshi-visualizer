625185658818f79e826adccdf74fd6cb073f7783
bakanayuzhouren-prog <bakanayuzhouren-prog@users.noreply.github.com>
2026-01-14 02:34:37 +0000

    deploy: 9f09cc13897ab559441ac1ffa89df7b0ff1ddbd9

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gyosei-shoshi-visualizer/sw.js', { scope: '/gyosei-shoshi-visualizer/' })})}
