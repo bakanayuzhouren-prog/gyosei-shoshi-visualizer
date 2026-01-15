4797cf74265e14210846126375610273daf578df
bakanayuzhouren-prog <bakanayuzhouren-prog@users.noreply.github.com>
2026-01-15 23:04:33 +0000

    deploy: 43d93fbed36a6f1c5d13a375152d6ae2b7c9b5cb

---
+++ assets/classDiagram-v2-WZHVMYZB-CVTuOFlC.js
@@ -0,0 +1,22 @@
+import { s as styles_default, c as classRenderer_v3_unified_default, a as classDiagram_default, C as ClassDB } from "./chunk-B4BG7PRW-DrdfG0-A.js";
+import { _ as __name } from "./index-DlmfdqdW.js";
+import "./chunk-FMBD7UC4-CNLh8dqg.js";
+import "./chunk-55IACEB6-DcZjMIw5.js";
+import "./chunk-QN33PNHL-ChHjubSK.js";
+var diagram = {
+  parser: classDiagram_default,
+  get db() {
+    return new ClassDB();
+  },
+  renderer: classRenderer_v3_unified_default,
+  styles: styles_default,
+  init: /* @__PURE__ */ __name((cnf) => {
+    if (!cnf.class) {
+      cnf.class = {};
+    }
+    cnf.class.arrowMarkerAbsolute = cnf.arrowMarkerAbsolute;
+  }, "init")
+};
+export {
+  diagram
+};
