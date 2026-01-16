380bd08ca717b6904026a9b9cac7382311d136fb
bakanayuzhouren-prog <bakanayuzhouren-prog@users.noreply.github.com>
2026-01-16 02:16:45 +0000

    deploy: e975cf5ebed34502cbb655cd485ba4259c99c9a8

--- assets/channel-DcdqAwuz.js
+++
@@ -1,7 +0,0 @@
-import { ap as Utils, aq as Color } from "./index-DS4c09Vg.js";
-const channel = (color, channel2) => {
-  return Utils.lang.round(Color.parse(color)[channel2]);
-};
-export {
-  channel as c
-};
