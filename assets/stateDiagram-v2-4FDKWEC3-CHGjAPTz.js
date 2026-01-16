6faa3a4f030a2efd4e2f8f6219561314368bfe8b
bakanayuzhouren-prog <bakanayuzhouren-prog@users.noreply.github.com>
2026-01-16 00:22:02 +0000

    deploy: 46b2c36d99b3f32d99a3d513627b69e8a2b53fbe

---
+++ assets/stateDiagram-v2-4FDKWEC3-CHGjAPTz.js
@@ -0,0 +1,21 @@
+import { s as styles_default, b as stateRenderer_v3_unified_default, a as stateDiagram_default, S as StateDB } from "./chunk-DI55MBZ5-BBRUyVwj.js";
+import { _ as __name } from "./index-DzjSs--F.js";
+import "./chunk-55IACEB6-CrpOLYwX.js";
+import "./chunk-QN33PNHL-BUG2sKM0.js";
+var diagram = {
+  parser: stateDiagram_default,
+  get db() {
+    return new StateDB(2);
+  },
+  renderer: stateRenderer_v3_unified_default,
+  styles: styles_default,
+  init: /* @__PURE__ */ __name((cnf) => {
+    if (!cnf.state) {
+      cnf.state = {};
+    }
+    cnf.state.arrowMarkerAbsolute = cnf.arrowMarkerAbsolute;
+  }, "init")
+};
+export {
+  diagram
+};
