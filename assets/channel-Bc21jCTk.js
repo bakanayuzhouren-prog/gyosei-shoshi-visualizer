0277602ffe342adf8592c1e3386fd2de974db777
bakanayuzhouren-prog <bakanayuzhouren-prog@users.noreply.github.com>
2026-01-18 06:44:06 +0000

    deploy: 54c61ef6133bab95c3d43e7cd2827f1cde4d716b

---
+++ assets/channel-Bc21jCTk.js
@@ -0,0 +1,7 @@
+import { ap as Utils, aq as Color } from "./index-tXUF3kR3.js";
+const channel = (color, channel2) => {
+  return Utils.lang.round(Color.parse(color)[channel2]);
+};
+export {
+  channel as c
+};
